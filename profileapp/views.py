5c6a1a9e4e6a75fd2f15b76e4872b03013243d3a
kovus380 <kosaf888@gmail.com>
2021-07-29 09:49:55 +0900

    07/29 class commit 2

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,13 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreateForm
 from profileapp.models import Profile
 
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileCreateForm
@@ -18,9 +23,11 @@ class ProfileCreateView(CreateView):
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
 
 
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
@@ -28,5 +35,4 @@ class ProfileUpdateView(UpdateView):
     template_name = 'profileapp/update.html'
 
     def get_success_url(self):
-        return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
-
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
