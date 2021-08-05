3e469619e5ee98aa1ea6977100fb1f4d3c92d90d
kovus380 <kosaf888@gmail.com>
2021-08-05 10:57:38 +0900

    08/05 class commit 4

--- articleapp/decorators.py
+++ articleapp/decorators.py
@@ -5,9 +5,9 @@ from articleapp.models import Article
 
 def article_ownership_required(func):
     def decorated(request, *args, **kwargs):
-        target_article = Article.objects.get(pk=kwargs[['pk']])
+        target_article = Article.objects.get(pk=kwargs['pk'])
         if target_article.writer == request.user:
             return func(request, *args, **kwargs)
         else:
             return HttpResponseForbidden()
-        return decorated
+    return decorated
