4f13d059b5028c27cf4beb26beaddbee77fead14
kovus380 <kosaf888@gmail.com>
2021-08-10 15:46:08 +0900

    08/10 class commit 3

--- commentapp/views.py
+++ commentapp/views.py
@@ -18,10 +18,10 @@ class CommentCreateView(CreateView):
         form.instance.article_id = self.request.POST.get('article_pk')
         return super().form_valid(form)
 
-
     def get_success_url(self):
         return reverse('articleapp:detail',
                        kwargs={'pk' : self.object.article.pk})
+
 
 class CommentDeleteView(DeleteView):
     model = Comment
