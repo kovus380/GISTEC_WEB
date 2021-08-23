0fe7178e0d35c24be634a6d68ea9295e1edb3220
kovus380 <kosaf888@gmail.com>
2021-08-23 09:24:08 +0900

    08/23 class commit 1

--- subscribeapp/models.py
+++ subscribeapp/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='subscription', null=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE,
+                                related_name='subscription')
+    # project에서 구독과 관련된 이름을 어떤식으로 가져올 건지 - related_name
+    # One user - One Subscription
+
+    # 외부 정보들
+    class Meta:
+        unique_together = ['user', 'project']
+        # 어떤 조합 쌍이 unique 하도록
+
+
